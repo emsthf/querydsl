0405d8706de0df8eb56ff066a2ff79bd5e41e657
emsthf <deunsol916@gmail.com>
2023-09-06 11:20:19 +0900

    feat: case문, 상수 더하기, 문자 더하기

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,8 @@
 package study.querydsl;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -509,6 +511,86 @@ public class QuerydslBasicTest {
 
         for (Tuple tuple : result) {
             System.out.println("tuple = " + tuple);
+        }
+
+        // then
+
+    }
+
+    @Test
+    void basicCase() throws Exception {
+        // given
+
+        // when
+        List<String> result = queryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타")
+                )
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+
+        // then
+
+    }
+
+    @Test
+    void complexCase() throws Exception {
+        // given
+
+        // when
+        List<String> result = queryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+
+        // then
+
+    }
+
+    @Test
+    void constant() throws Exception {
+        // given
+
+        // when
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+
+        // then
+
+    }
+
+    @Test
+    void concat() throws Exception {
+        // given
+
+        // when
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))  // stringValue()를 사용하면 문자열로 변환해서 concat을 사용할 수 있다.
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
         }
 
         // then
